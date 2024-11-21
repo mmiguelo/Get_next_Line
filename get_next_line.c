1e4128c823a28f07eb2b6559cd0cb9056101e965
Marco Alexandre Miguelote Santos <mmiguelo@c1r2s2.42porto.com>
2024-11-21 15:44:11 +0000

    tried to fix save remaining

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mmiguelo <mmiguelo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 11:02:46 by mmiguelo          #+#    #+#             */
-/*   Updated: 2024/11/20 19:15:22 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2024/11/21 15:24:37 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,14 @@ char	*read_text(int fd, char *storage)
 	if (!buffer)
 		return (NULL);
 	nbytes = 1;
-	while (nbytes != 0)
+	while (nbytes != 0 && !strchr(storage, '\n'))
 	{
 		nbytes = read(fd, buffer, BUFFER_SIZE);
-		if (nbytes == 0 || nbytes == -1)
+		if (nbytes <= 0)
 		{
-			return (NULL);
 			free (buffer);
+			free (storage);
+			return (NULL);
 		}
 		storage = ft_strjoin(storage, buffer);
 		buffer[nbytes] = '\0';
@@ -46,25 +47,19 @@ char	*cut_string(char *storage)
 
 	i = 0;
 	j = 0;
-	if (!storage[i] || !storage)
+	if (!storage || storage[0] == '\0')
 		return (NULL);
-	while (storage[i] != '\n' && storage[i])
+	while (storage[i] != '\n' && storage[i] != '\0')
 		i++;
-	new = malloc(sizeof(char) * i + 2);
+	new = malloc(i + 2);
+	if (!new)
+		return (NULL);
 	while (j < i + 1)
 	{
 		new[j] = storage[j];
 		j++;
 	}
 	new[j] = '\0';
-	// while (storage[i] && storage[i] != '\n')
-	// {
-		// new[i] = storage[i];
-		// i++;
-	// }
-	// if (storage[i] && storage[i] == '\n')
-		// new[i++] = '\n';
-	// new[i] = '\0';
 	return (new);
 }
 
@@ -76,9 +71,14 @@ char	*save_remaining(char *storage)
 	
 	i = ft_strlen(storage, '\n');
 	j = ft_strlen(storage, '\0');
+	if (i == j)
+		return (NULL);
 	temp = malloc(sizeof(char) * (j - i));
+	if (!temp)
+		return (NULL);
 	j = 0;
-	while (storage[++i] != '\0')
+	i++;
+	while (storage[i] != '\0')
 	{
 		temp[j] = storage[i];
 		i++;
@@ -98,23 +98,26 @@ char	*get_next_line(int fd)
 	if (!storage)
 	{
 		storage = malloc(sizeof(char) * 1);
+		if (!storage)
+			return (NULL);
 		storage[0] = '\0';
 	}
 	storage = read_text(fd, storage);
-	if (!storage)
-		return (NULL);
 	result = cut_string(storage);
 	storage = save_remaining(storage);
 	return (result);
 }
 
-int	main(void)
+/* int	main(void)
 {
 	char *line;
 	int fd = open("test.txt", O_RDONLY);
 	line = get_next_line(fd);
-	printf("%s", line);
-	free(line);
-	line = get_next_line(fd);
+	while (line)
+	{
+		printf("%s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
 	return (0);
-}
+} */
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: mmiguelo <mmiguelo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 11:03:04 by mmiguelo          #+#    #+#             */
-/*   Updated: 2024/11/20 17:21:32 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2024/11/21 11:55:44 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlen(char *str, char c)
 {
 	size_t	i;
 
+	if (!str)
+		return (0);
 	i = 0;
 	while (str[i] != c)
 		i++;
@@ -26,6 +28,7 @@ char	*ft_strlcpy(char *dst, char *src, size_t size)
 	size_t	i;
 	size_t	length;
 
+	(void)length;
 	if (!dst || !src)
 		return (0);
 	length = ft_strlen(src, '\0');
@@ -44,12 +47,14 @@ char	*ft_strlcpy(char *dst, char *src, size_t size)
 char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*new;
-	int		s1_length;
-	int		s2_length;	
-
+	size_t		s1_length;
+	size_t		s2_length;	
+	
+	if (!s1 || !s2)
+		return (NULL);
 	s1_length = ft_strlen(s1, '\0');
 	s2_length = ft_strlen(s2, '\0');
-	new = calloc((s1_length + s2_length +1), sizeof(char));
+	new = malloc(sizeof(char) * (s1_length + s2_length +1));
 	if (!new)
 		return (NULL);
 	ft_strlcpy(new, s1, s1_length + 1);
